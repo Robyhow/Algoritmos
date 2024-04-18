bfd1c6d8f2be6a2e8c65b8ea2001160c7210b9fb
kayky j <167081620+kaykyjacome@users.noreply.github.com>
2024-04-17 22:45:18 -0300

    Update calculadora.c
    Mensagens Claras: Adicionei mensagens mais informativas para orientar o usuário durante a entrada e a saída.
    Limitação do Operador para Entrada Única: Modifiquei a mensagem de entrada para que o usuário insira apenas o operador desejado, sem a necessidade de digitar "Digite a operação (+, -, *, /):".
    Precisão de Saída: Ajustei a precisão de saída para exibir o resultado com duas casas decimais.
    Remoção de Repetições: Simplifiquei o código removendo a repetição da linha printf("Resultado: %.2f\n", resultado);.

--- calculadora.c
+++ calculadora.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main() {
     char operador;
     float num1, num2, resultado;
 
-    printf("Digite a operação (+, -, *, /): ");
+    printf("Bem-vindo à calculadora simples!\n");
+    printf("Por favor, selecione a operação desejada (+, -, *, /): ");
     scanf(" %c", &operador);
 
-    printf("Digite dois números: ");
+    printf("Digite os dois números: ");
     scanf("%f %f", &num1, &num2);
 
     switch (operador) {
         case '+':
             resultado = num1 + num2;
-            printf("Resultado: %.2f\n", resultado);
             break;
         case '-':
             resultado = num1 - num2;
-            printf("Resultado: %.2f\n", resultado);
             break;
         case '*':
             resultado = num1 * num2;
-            printf("Resultado: %.2f\n", resultado);
             break;
         case '/':
             if (num2 != 0) {
                 resultado = num1 / num2;
-                printf("Resultado: %.2f\n", resultado);
             } else {
                 printf("Erro! Divisão por zero.\n");
+                return 1; // Encerra o programa com código de erro
             }
             break;
         default:
             printf("Operador inválido.\n");
+            return 1; // Encerra o programa com código de erro
     }
+
+    printf("Resultado: %.2f\n", resultado);
 
     return 0;
 }
